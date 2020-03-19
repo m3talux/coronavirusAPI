0ae68418e33680480903ff0ae8877be9add6d696
Alex Khoury <alex@polarstork.com>
2020-03-19 06:07:44 +0200

    Changed db host key

--- app/models/db.js
+++ app/models/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: process.env.CLEARDB_DATABASE_URL,
+    host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DB
