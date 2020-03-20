be17f279f157c3bed094b1281a5b1f4fd7948405
Alex Khoury <53697093+alex-khoury@users.noreply.github.com>
2020-03-20 07:29:59 +0200

    Converted sql connection to pool

--- app/models/db.js
+++ app/models/db.js
@@ -1,15 +1,23 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
+    connectionLimit : 10,
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DB
 });
 
-connection.connect(error => {
-    if (error) throw  error;
-    console.log('Successfully connected to the database');
+pool.on('acquire', function (connection) {
+  console.log('Connection %d acquired', connection.threadId);
 });
 
-module.exports = connection;
+pool.on('enqueue', function () {
+  console.log('Waiting for available connection slot');
+});
+
+pool.on('release', function (connection) {
+  console.log('Connection %d released', connection.threadId);
+});
+
+module.exports = pool;
