18bb27a5a87640815ac5c0c77ba8015244cb39ab
Alex Khoury <alex@polarstork.com>
2020-03-21 18:43:01 +0200

    Reinforced algorithm for expectation + added outlook value

--- www/index.js
+++ www/index.js
@@ -99,23 +99,59 @@ $(function () {
     function getTodayExpectedCases(result) {
         if (result.length > 1) {
             const lastCasesValue = result[result.length - 2].cases;
-            let variationPercentages = [];
+            const variationPercentages = [];
             let totalVariation = 0;
+            const step = 0.1;
+            let importance = 1.0;
             for (let i = 1; i < result.length - 2; i++) {
+                const outlook = getTodayOutlook(variationPercentages);
                 variationPercentages[i - 1] =
-                    (Math.abs(result[i].cases - result[i + 1].cases) / result[i].cases) * (i / 10) * 100;
+                    (Math.abs(result[i].cases - result[i + 1].cases) / result[i].cases) * outlook * importance * 100;
                 totalVariation += variationPercentages[i - 1];
+                importance += step;
             }
+            console.log(variationPercentages);
+            const outlook = getTodayOutlook(variationPercentages);
             const percentageIncrease = totalVariation / variationPercentages.length;
-            $('#today-expected-cases').html(Math.round(lastCasesValue + (lastCasesValue * (totalVariation / variationPercentages.length) / 100)));
+
+            $('#today-expected-cases').html(Math.round(lastCasesValue + (lastCasesValue * percentageIncrease / 100)));
             if (percentageIncrease > 0) {
                 $('#today-expected-cases-percentage').addClass('negative')
-                $('#today-expected-cases-percentage').html(' (+' + (totalVariation / variationPercentages.length).toFixed(2) + '%)');
+                $('#today-expected-cases-percentage').html(' (+' + percentageIncrease.toFixed(2) + '%)');
             } else {
                 $('#today-expected-cases-percentage').addClass('positive')
-                $('#today-expected-cases-percentage').html(' (' + (totalVariation / variationPercentages.length).toFixed(2) + '%)');
+                $('#today-expected-cases-percentage').html(' (' + percentageIncrease.toFixed(2) + '%)');
             }
+            if (outlook > 0.9 && outlook < 1.1) {
+                $('#today-outlook').html('NEUTRAL');
+                $('#today-outlook').addClass('neutral');
+                $('#today-outlook-value').addClass('neutral');
+            } else if (outlook >= 1.1) {
+                $('#today-outlook').html('NEGATIVE');
+                $('#today-outlook').addClass('negative');
+                $('#today-outlook-value').addClass('negative');
+            } else if (outlook <= 0.9) {
+                $('#today-outlook').html('POSITIVE');
+                $('#today-outlook').addClass('positive');
+                $('#today-outlook-value').addClass('positive');
+            }
+            $('#today-outlook-value').html('(' + (outlook >= 0 ? '+' : '') + outlook.toFixed(2) + ')');
         }
+    }
+
+    function getTodayOutlook(array) {
+        if (array.length > 1) {
+            let outlook = 1.0;
+            const step = 1 / array.length;
+            for (let i = 1; i < array.length; i++) {
+                if (array[i] > array [i - 1]) {
+                    outlook += step;
+                } else if (array[i] < array[i - 1]) {
+                    outlook -= step;
+                }
+            }
+            return outlook;
+        } else return 1;
     }
 
     function getWorldwideChartData(data) {
