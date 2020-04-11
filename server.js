2c4f02ea3ef268f33f8a894c2b68b3405cc4752c
Alex Khoury <53697093+luxation@users.noreply.github.com>
2020-04-11 06:17:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -209,7 +209,7 @@ const getAllStatisticsPerCountry = setInterval(async () => {
         const countryController = require('./app/controllers/country.controller');
         countryController.createOrUpdate(res);
     }
-}, 600000);
+}, 60000);
 
 require('./app/routes/worldwide.routes')(app);
 require('./app/routes/country.routes')(app);
